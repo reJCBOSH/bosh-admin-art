ab5bb6971b7a963dfd7c1f3a1751f3e0a5f54695
JCBOSH <942419633@qq.com>
2025-11-14 14:45:48 +0800

    refactor: 修改分页参数映射

--- src/utils/table/tableConfig.ts
+++ src/utils/table/tableConfig.ts
@@ -48,8 +48,8 @@ export const tableConfig = {
   // useTable 组合式函数传递分页参数的时候 用 current 跟 size
   paginationKey: {
     // 当前页码
-    current: 'current',
+    current: 'pageNo',
     // 每页大小
-    size: 'size'
+    size: 'pageSize'
   }
 }
